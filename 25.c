6c86152c7be0289747c3dd541bff2c2497d10745
RushilKhatri <86219945+RushilKhatri@users.noreply.github.com>
2025-11-05 21:33:23 +0530

    25.c

--- 25.c
+++ 25.c
@@ -2,7 +2,15 @@
 ============================================================================
 Name : 25.c
 Author : Rushil Khatri
-Description : Write a program to create a message queue and print the key and message queue id.
+Description : Write a program to print a message queue's (use msqid_ds and ipc_perm structures)
+ a. access permission
+ b. uid, gid
+ c. time of last message sent and received
+ d. time of last change in the message queue
+ d. size of the queue
+ f. number of messages in the queue
+ g. maximum number of bytes allowed
+ h. pid of the msgsnd and msgrcv
 Date: 20th October 2025
 ============================================================================
 */
